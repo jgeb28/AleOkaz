e5af33c05c1c0a7d9bfe6d2ef76ae37bfaebcc77
Michał Ciechan <michal.ciechan.stud@pw.edu.pl>
2024-11-29 23:52:47 +0100

    Backend: Add User

---
+++ backend/src/main/java/pl/aleokaz/backend/user/User.java
@@ -0,0 +1,56 @@
+package pl.aleokaz.backend.user;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Entity
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NonNull
+    @Column(unique = true)
+    private String email;
+
+    @NonNull
+    @Column(unique = true)
+    private String username;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    @UniqueElements
+    @CollectionTable(name = "user_role")
+    @Column(name = "role")
+    private Set<UserRole> roles;
+
+    @Builder
+    public User(UUID id, String email, String username, String password, Set<UserRole> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = new HashSet<>(roles);
+    }
+}
