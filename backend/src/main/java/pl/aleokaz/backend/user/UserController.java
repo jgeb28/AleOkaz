86925084a5339dee59fe747ea4a2f02ac2af46ee
Michał Ciechan <michal.ciechan.stud@pw.edu.pl>
2024-12-14 22:21:33 +0100

    Backend: Add UserController#findUserById

--- backend/src/main/java/pl/aleokaz/backend/user/UserController.java
+++ backend/src/main/java/pl/aleokaz/backend/user/UserController.java
@@ -1,20 +1,38 @@
 package pl.aleokaz.backend.user;
 
+import java.net.URISyntaxException;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+// TODO(michalciechan): Obsługa wyjątków.
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> findUserById(@RequestParam UUID id) {
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
+
     @PostMapping
-    public ResponseEntity<UserDto> registerUser(@RequestBody RegisterCommand registerCommand) {
-        return ResponseEntity.ok(userService.registerUser(registerCommand));
+    public ResponseEntity<UserDto> registerUser(@RequestBody RegisterCommand registerCommand)
+            throws URISyntaxException {
+        final var user = userService.registerUser(registerCommand);
+        final var uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
